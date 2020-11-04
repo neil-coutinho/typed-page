494eee61f323c4486c9b1a9afdc28b570208c32f
Neil Coutinho <njcoutinho@gmail.com>
2020-11-04 16:24:44 +0530

    Random wait times

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+function getRandomBetween(min=0, max=150) {
+    return Math.floor(Math.random()*(max-min) + min);
+}
 
 async function render() {
-    console.log('ready to begin typing');
+    //console.log('ready to begin typing');
     const paragraphs = document.querySelectorAll("[data-typed]");
-   
-    for(const p of paragraphs) {
-        await wait(1000);
+
+
+    for (const p of paragraphs) {
+        const ms = getRandomBetween(150);
+       
+        await wait(ms);
+        p.classList.remove('hide');
         let result = await typeIt(p)
     }
 }
 
 
-function typeIt(p) {
-    console.log(p)
+async function typeIt(p) {
+    const text = p.textContent;
+
+    let sentence = '';
+
+    for (const letter of text) {
+        const lms = getRandomBetween(50);
+       
+        sentence += letter;
+
+        p.textContent = sentence;
+        await wait(lms);
+    }
 }
 
 
